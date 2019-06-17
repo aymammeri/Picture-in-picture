3ff048c1c57a4ffaecdfad2e3b69b8140e699b74
Ayoub Mammeri <ay.mammeri@gmail.com>
2019-06-17 16:01:08 -0400

    Add Picture in picture functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-// Promp to select media stream , pass to video element, than play
+// Promp to select media stream, pass to video element, than play
 async function selectMediaStream() {
     try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
@@ -14,5 +14,13 @@ async function selectMediaStream() {
     }
 }
 
+button.addEventListener('click', async () => {
+    // Disable button
+    button.disable = true;
+    // Start picture in picture 
+    await videoElement.requestPictureInPicture();
+    // Enable button
+    button.disable = false;
+});
 // Onload
 selectMediaStream();
